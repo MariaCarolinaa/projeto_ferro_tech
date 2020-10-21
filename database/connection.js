b7930ac1e2839d93f4b71e279fd991fae9a8231e
MariaCarolinaa <50643879+MariaCarolinaa@users.noreply.github.com>
2020-10-20 21:51:31 -0300

    banco de produção

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,9 @@
 var knex = require('knex')({
     client: 'mysql2',
     connection: {
-      host : '127.0.0.1',
-      user : 'root',
-      password : '',
+      host : 'mysql669.umbler.com',
+      user : 'adminferrotech',
+      password : 'Laboratorio2020',
       database :  'ferrotech'
     }
   });
